2755c1fac464eb1cf62e2b5b112ed27268e28345
Alex M. Telezhkin <telezhkin@gmail.com>
2016-11-17 20:20:40 +0000

    1

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-def hello(environ, start_respopnse):
+import urlparse
+def hello(environ, start_response):
 	
+	d = urlparse.parse_qs(environ["QUERY_STRING"])
 	status = "200 OK"
 	headers = [
 		('Content-type', 'text/plain')
 	]
-	body = "Hello world"
+	body = ""
+	for k in sorted(d.iterkeys()):
+		body += k + "="
+		body += "=".join(d[k]) + "\n"
 	start_response(status, headers)
 	return [ body ]
 
