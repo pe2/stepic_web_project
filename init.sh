7e1a5c39f89bf1eabceb982e8e7c22cb03907cc6
Alex M. Telezhkin <telezhkin@gmail.com>
2016-11-17 13:38:53 +0000

    edit init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+git config --global user.email "telezhkin@gmail.com"
+git config --global user.name "Alex M. Telezhkin"
