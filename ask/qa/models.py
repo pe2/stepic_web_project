0da055d1e8a9daee96b4d1c3c84846eda405e5e6
Alex M. Telezhkin <telezhkin@gmail.com>
2016-11-24 08:47:00 +0000

    news

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+	def new():
+		pass
+	def popular():
+		pass
+
+class Question(models.Model):
+	objects = QuestionManager()
+	title=models.CharField(max_length=255)
+	text=models.TextField()
+	added_at=models.DateField()
+	rating=models.IntegerField()
+	author=models.OneToOneField(User)
+	likes=models.ManyToManyField(User, related_name="likes")
+
+class Answer(models.Model):
+	text=models.TextField()
+	added_ad=models.DateField()
+	question=models.ForeignKey(Question)
+	author=models.OneToOneField(User)
