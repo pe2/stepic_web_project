48c294bc0dee869b18c400e0893b5774f3eceff4
Alex M. Telezhkin <telezhkin@gmail.com>
2016-11-24 09:24:09 +0000

    done

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,13 +12,13 @@ class Question(models.Model):
 	objects = QuestionManager()
 	title=models.CharField(max_length=255)
 	text=models.TextField()
-	added_at=models.DateField()
-	rating=models.IntegerField()
-	author=models.OneToOneField(User)
-	likes=models.ManyToManyField(User, related_name="likes")
+	added_at=models.DateField(blank=True, auto_now_add=True)
+	rating=models.IntegerField(default=0)
+	author=models.ForeignKey(User)
+	likes=models.ManyToManyField(User, related_name="likes_set")
 
 class Answer(models.Model):
 	text=models.TextField()
-	added_ad=models.DateField()
+	added_at=models.DateField(blank=True, auto_now_add=True)
 	question=models.ForeignKey(Question)
 	author=models.OneToOneField(User)
